e759f24b3784cb488de5d4ddaa14c44f947918f5
Jacob Dichter <58149811+jacobdichter@users.noreply.github.com>
2024-06-18 14:01:56 -0400

    Add files via upload

---
+++ basic_flask_app.py
@@ -0,0 +1,13 @@
+#-- pip install flask
+#-- pip install Flask-HTTPAuth
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello, world"
+
+if __name__ == '__main__':
+    app.run(debug=True)
